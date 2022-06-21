1a1d9f98fc1ef96d6f9740b1f077747a23a9d436
RODF10 <80124832+RODF10@users.noreply.github.com>
2022-06-21 08:44:57 -0500

    Add files via upload

---
+++ Ejercicios 2P/C#/MovilBN/MovilBN/MovilBN/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MovilBN
+{
+    public partial class MainPage : ContentPage
+    {
+        int contador;
+        public MainPage()
+        {
+            InitializeComponent();
+            contador = 0;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            contador++;
+            ((Button)sender).Text = "Presionado!!! "+contador+" veces";
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
